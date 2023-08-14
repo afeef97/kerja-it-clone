20355d28742d13312d0e35d4eb1952ef02faba14
Afeef <afeef9706@gmail.com>
2023-08-14 14:55:39 +0800

    Added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />,
+        },
+    ]);
+
     return (
         <div>
-            <h1>Hello world</h1>
+            <RouterProvider router={router} />
         </div>
     );
 }
